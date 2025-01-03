78f7daaedab31fce21c71f05b6ae4477c274aab7
Krishnagokul1305 <krishnagokul1729@gmail.com>
2025-01-03 20:24:58 +0530

    added loader

---
+++ app/(root)/loading.jsx
@@ -0,0 +1,10 @@
+function Loading() {
+    return (
+      <div className="h-full flex justify-center items-center py-20">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>
+      </div>
+    );
+  }
+  
+  export default Loading;
+  
